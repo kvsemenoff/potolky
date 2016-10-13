64c8d09959423df5cce703072ac8901c52cf6027
hatuhov <hatuhov@yandex.ru>
2016-10-13 16:37:46 +0300

    r

--- includes/header-bottom.php
+++ includes/header-bottom.php
@@ -1,1 +1,24 @@
-header-bottom.php
+
+<div class="clearfix"></div>
+
+<section class="ah-bottomhead">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-bottomhead-box">
+					<div class="ah-bottomhead-box1">
+						<span class="ah-bottomhead-box1-span">Множество вариантов</span> <!-- т.к. между ними отступы -->
+						<span class="ah-bottomhead-box1-span">исполнения</span><!-- т.к. между ними отступы -->
+						<span class="ah-bottomhead-box1-span">потолка</span>
+					</div>	
+				</div>
+				<div class="ah-bottomhead-info">
+					<p>При строительстве в роли потолка в помещениях чаще всего выступает бетонная плита, служащая разделителем между этажами. Таким образом, нижняя её часть становится потолком, верхняя полом верхнего этажа ...</p>
+					<a href="#">о портале</a>
+				</div>	
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="clearfix"></div>
