06dcca77db11994506a231c26c11ecd72dd60093
engine77 <zaur.ka.00@mail.ru>
2016-10-13 13:47:43 +0300

    1

--- includes/fotografii.php
+++ includes/fotografii.php
@@ -1,1 +1,14 @@
-fotografii.php
+<section class="zk_photos">
+	<div class="container">
+		<div class="row">		   
+		   		<div class="zk_photos">
+		   			<h3>Фотографии</h3>
+		   			<div class="zk_photo"><img src="../images/photo1.png" alt=""></div>
+		   			<div class="zk_photo"><img src="../images/photo2.png" alt=""></div>
+		   			<div class="zk_photo"><img src="../images/photo3.png" alt=""></div>
+		   			<div class="zk_photo"><img src="../images/photo4.png" alt=""></div>
+		   			<div class="zk_photo"><img src="../images/photo5.png" alt=""></div>
+		   		</div>	   
+		</div>
+	</div>
+</section>		   	
