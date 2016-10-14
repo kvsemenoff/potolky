db5c9ca43e6d3be47f91bb337e35af3c9b285114
unknown <anzarsh@mail.ru>
2016-10-14 17:33:00 +0400

    sdfs

--- includes/tovari.php
+++ includes/tovari.php
@@ -1,41 +1,57 @@
-<div class="az-tovari">
+<div class="az-tovari az-margin-top70">
 	<div class="az-tovari-lr">
 		<div class="az-tovari-tb">
 			<div class="az-margin-top--20px">
-				<span class="az-tovari-title"></span>
-
-			</div>
-			<div class="az-col1">
-				<div class="az-tovar">
-					<div class="az-img-box2">
-						<img src="img/az-cat2.jpg" alt="">
-					</div>
-					<div class="az-tovar-title az-margin-top10">Крашенные потолки</div>
-					<div class="az-tovar-text az-margin-top10">Крашенные потолки</div>
-					<div class="az-tovar-price az-margin-top10">Крашенные потолки</div>
+				<div class="az-tov-head1">
+					<span class="az-tovari-title">Товары</span>
 				</div>
-			</div>
-			<div class="az-col1">
-				<div class="az-tovar">
-					<div class="az-img-box2">
-						<img src="img/az-cat2.jpg" alt="">
-					</div>
-					<div class="az-tovar-title az-margin-top10">Крашенные потолки</div>
-					<div class="az-tovar-text az-margin-top10">Крашенные потолки</div>
-					<div class="az-tovar-price az-margin-top10">Крашенные потолки</div>
+				<div class="az-tov-head2">
+					<a href="#" class="az-look">посмотреть все</a>
 				</div>
-			</div>
-			<div class="az-col1">
-				<div class="az-tovar">
-					<div class="az-img-box2">
-						<img src="img/az-cat2.jpg" alt="">
-					</div>
-					<div class="az-tovar-title az-margin-top10">Крашенные потолки</div>
-					<div class="az-tovar-text az-margin-top10">Крашенные потолки</div>
-					<div class="az-tovar-price az-margin-top10">Крашенные потолки</div>
+				<div class="az-add">
+					<a href="#" >Добавить товар</a>
 				</div>
+				<div class="clearfix"></div>
 			</div>
-			<div class="clearfix"></div>
+			<div>
+				<div class="az-col1">
+					<div class="az-tovar">
+						<div class="az-img-box_style">
+							<div class="az-img-box2">
+								<img src="img/tovar1.jpg" alt="">
+							</div>
+						</div>
+						<div class="az-tovar-title az-margin-top16">Шпатель</div>
+						<div class="az-tovar-text az-margin-top10">Магазин Эва, Москва</div>
+						<div class="az-tovar-price az-margin-top10"><strong>195</strong>р</div>
+					</div>
+				</div>
+				<div class="az-col1">
+					<div class="az-tovar">
+						<div class="az-img-box_style">
+							<div class="az-img-box2">
+								<img src="img/tovar2.jpg" alt="">
+							</div>
+						</div>
+						<div class="az-tovar-title az-margin-top16">Ткань потолочная эко ПВХ</div>
+						<div class="az-tovar-text az-margin-top10">Магазин Эва, Москва</div>
+						<div class="az-tovar-price az-margin-top10"><strong>290</strong>р/м</div>
+					</div>
+				</div>
+				<div class="az-col1">
+					<div class="az-tovar">
+						<div class="az-img-box_style">
+							<div class="az-img-box2">
+								<img src="img/tovar3.jpg" alt="">
+							</div>
+						</div>
+						<div class="az-tovar-title az-margin-top16">Монтажный клей</div>
+						<div class="az-tovar-text az-margin-top10">Магазин Эва, Москва</div>
+						<div class="az-tovar-price az-margin-top10"><strong>95</strong>р/шт</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
 		</div>
 	</div>
 </div>
