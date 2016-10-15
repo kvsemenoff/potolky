e9dc052683c861a4a94ef8964d6f6b84eee5f6cc
Berzeg07 <berezg86@gmail.com>
2016-10-15 17:22:10 +0400

    10.10

--- single.php
+++ single.php
@@ -308,27 +308,127 @@
 							<div class="ab-single-wrap ab-single-wrap-2">
 								<div class="ab-single-txt">
 									<span>
-										Никакого жесткого крепления у них нет. Как же клеить потолочный плинтус? Никак. Этого делать нельзя. И на то есть несколько причин. 
+										Деревянные галтели фиксируются на крепежных элементах, закрепляемых на стене. Для них также отмеряется нужное расстояние от натяжного потолка. После установки крепежных элементов к ним прибивается непосредственно плинтус.
 									</span>
 
 								</div>
 								<div class="az-center3">
 									<div class="az-stars">
-										<input type="radio" name="radio1" id="a0" class="radio0">
-										<input type="radio" name="radio1" id="a1" class="radio1">
-										<input type="radio" name="radio1" id="a2" class="radio2">
-										<input type="radio" name="radio1" id="a3" class="radio3">
-										<input type="radio" name="radio1" id="a4" class="radio4">
-										<input type="radio" name="radio1" id="a5" class="radio5">
-										<label for="a0" class="az-none"></label>
-										<label for="a1" class="label1"></label>
-										<label for="a2" class="label2"></label>
-										<label for="a3" class="label3"></label>
-										<label for="a4" class="label4"></label>
-										<label for="a5" class="label5"></label>
+										<input type="radio" name="radio1" id="a18" class="radio0">
+										<input type="radio" name="radio1" id="a19" class="radio1">
+										<input type="radio" name="radio1" id="a20" class="radio2">
+										<input type="radio" name="radio1" id="a21" class="radio3">
+										<input type="radio" name="radio1" id="a22" class="radio4">
+										<input type="radio" name="radio1" id="a23" class="radio5">
+										<label for="a18" class="az-none"></label>
+										<label for="a19" class="label1"></label>
+										<label for="a20" class="label2"></label>
+										<label for="a21" class="label3"></label>
+										<label for="a22" class="label4"></label>
+										<label for="a23" class="label5"></label>
 									</div>
 	
 									<div class="clearfix"></div>
+								</div>
+							</div>
+							<div class="ab-single-wrap .ab-single-wrap-10">
+								<div class="ab-publish ab-publish-2">
+									<span>Виктория Фамина Опубликовано 20.01.2016</span>
+								</div>
+							</div>
+							<div class="ab-single-wrap ab-single-wrap-2">
+								<div class="ab-single-txt">
+									<span>
+										Полиуретановые изделия используются очень часто. Они достаточно гибкие и долговечные. Но есть у них и недостатки — это их вес и небольшая толщина, т.е. будет меньше площадь шва.
+									</span>
+								</div>
+								<div class="az-center3">
+									<div class="az-stars">
+										<input type="radio" name="radio1" id="a12" class="radio0">
+										<input type="radio" name="radio1" id="a13" class="radio1">
+										<input type="radio" name="radio1" id="a14" class="radio2">
+										<input type="radio" name="radio1" id="a15" class="radio3">
+										<input type="radio" name="radio1" id="a16" class="radio4">
+										<input type="radio" name="radio1" id="a17" class="radio5">
+										<label for="a12" class="az-none"></label>
+										<label for="a13" class="label1"></label>
+										<label for="a14" class="label2"></label>
+										<label for="a15" class="label3"></label>
+										<label for="a16" class="label4"></label>
+										<label for="a17" class="label5"></label>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+							<div class="ab-single-wrap">
+								<div class="row">
+									<div class="col-md-4 col-sm-12 col-xs-12">
+										<div class="ab-span-input">
+											<span>Ваше имя</span>
+										</div>
+										<div class="vhod-bottom">
+											<div class="search-box">
+												<form class="ab-f" action="#">
+													<label class="ab-first-input">
+														<input type="text" placeholder="floesdesign">
+													</label>
+												</form>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-4 col-sm-12 col-xs-12">
+										<div class="ab-span-input">
+											<span>Ваше e-mail</span>
+										</div>
+										<div class="vhod-bottom">
+											<div class="search-box">
+												<form class="ab-f" action="#">
+													<label class="ab-second-input">
+														<input type="text" placeholder="">
+													</label>
+												</form>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="ab-podpis">
+									<span>Например Саша Викторов</span>
+								</div>
+								<div class="row">
+									<div class="col-md-12 col-sm-12 col-xs-12">
+										<div class="vhod-bottom vhod-bottom-2">
+											<div class="search-box">
+												<form action="#">
+													<label>
+														<textarea class="ab-ff" placeholder=""></textarea>
+													</label>
+												</form>
+											</div>
+										</div>
+									</div>
+									<div class="clearfix"></div>
+									<div class="col-md-6 col-sm-12 col-xs-12">
+										<div class="ab-span-input ab-span-input-2">
+											<span>Ваша оценка статьи</span><span>Не обязательно</span>
+										</div>
+											<div class="az-center az-center2 az-center5">
+									<div class="az-stars">
+										<input type="radio" name="radio1" id="a6" class="radio0">
+										<input type="radio" name="radio1" id="a7" class="radio1">
+										<input type="radio" name="radio1" id="a8" class="radio2">
+										<input type="radio" name="radio1" id="a9" class="radio3">
+										<input type="radio" name="radio1" id="a10" class="radio4">
+										<input type="radio" name="radio1" id="a11" class="radio5">
+										<label for="a6" class="az-none"></label>
+										<label for="a7" class="label1"></label>
+										<label for="a8" class="label2"></label>
+										<label for="a9" class="label3"></label>
+										<label for="a10" class="label4"></label>
+										<label for="a11" class="label5"></label>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+									</div>
 								</div>
 							</div>
 						</div>
