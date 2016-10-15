24f77e0c53173d824f7188b47d9c31281a950846
unknown <anzarsh@mail.ru>
2016-10-15 15:21:01 +0400

    Каталог компаний

--- includes/catologcomp.php
+++ includes/catologcomp.php
@@ -12,70 +12,188 @@
 			</div>
 			<div class="az-firm-items">
 				<div class="az-col3">
-					<div class="az-firm-left">
-						<div class="az-img-box_style">
-							<div class="az-img-box2">
+					<div class="az-firm-item az-margin-bottom40 az-zapis-border az-zapis-border_style2">
+						<div class="az-firm-left">
+							<div class="az-img-box2 az-img-box2_style2">
 								<img src="img/comp1.jpg" alt="">
 							</div>
 						</div>
+						<div class="az-firm-right">
+							<span class="az-firm-right1 az-margin-top8 az-xs-margin-top0">Потолок Эверест</span>
+							<span class="az-firm-right2 az-margin-top8">
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-firm-right2-inner">Средняя оценка 4.0</span>
+							</span>
+							<span class="az-firm-right3 az-margin-top8">Москва ул. Савельево 230 офис 32</span>
+							<span class="az-firm-right4 az-margin-top8">8 800 900-30-40</span>
+						</div>
+						<div class="clearfix"></div>
 					</div>
-					<div class="az-firm-right"><br></div>
-					<div class="clearfix"></div>
 				</div>
 				<div class="az-col3">
-					<div class="az-firm-left">
-						<div class="az-img-box_style">
-							<div class="az-img-box2">
-								<img src="img/comp1.jpg" alt="">
+					<div class="az-firm-item az-margin-bottom40 az-zapis-border az-zapis-border_style2">
+						<div class="az-firm-left">
+							<div class="az-img-box2 az-img-box2_style2">
+								<img src="img/comp2.jpg" alt="">
 							</div>
 						</div>
+						<div class="az-firm-right">
+							<span class="az-firm-right1 az-margin-top8 az-xs-margin-top0">Потолок Эверест</span>
+							<span class="az-firm-right2 az-margin-top8">
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-firm-right2-inner">Средняя оценка 4.0</span>
+							</span>
+							<span class="az-firm-right3 az-margin-top8">Москва ул. Савельево 230 офис 32</span>
+							<span class="az-firm-right4 az-margin-top8">8 800 900-30-40</span>
+						</div>
+						<div class="clearfix"></div>
 					</div>
-					<div class="az-firm-right"><br></div>
-					<div class="clearfix"></div>
 				</div>
 				<div class="az-col3">
-					<div class="az-firm-left">
-						<div class="az-img-box_style">
-							<div class="az-img-box2">
-								<img src="img/comp1.jpg" alt="">
+					<div class="az-firm-item az-margin-bottom40 az-zapis-border az-zapis-border_style2">
+						<div class="az-firm-left">
+							<div class="az-img-box2 az-img-box2_style2">
+								<img src="img/comp3.jpg" alt="">
 							</div>
 						</div>
+						<div class="az-firm-right">
+							<span class="az-firm-right1 az-margin-top8 az-xs-margin-top0">Потолок Эверест</span>
+							<span class="az-firm-right2 az-margin-top8">
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-firm-right2-inner">Средняя оценка 4.0</span>
+							</span>
+							<span class="az-firm-right3 az-margin-top8">Москва ул. Савельево 230 офис 32</span>
+							<span class="az-firm-right4 az-margin-top8">8 800 900-30-40</span>
+						</div>
+						<div class="clearfix"></div>
 					</div>
-					<div class="az-firm-right"><br></div>
-					<div class="clearfix"></div>
 				</div>
 				<div class="az-col3">
-					<div class="az-firm-left">
-						<div class="az-img-box_style">
-							<div class="az-img-box2">
+					<div class="az-firm-item az-margin-bottom40 az-zapis-border az-zapis-border_style2">
+						<div class="az-firm-left">
+							<div class="az-img-box2 az-img-box2_style2">
 								<img src="img/comp1.jpg" alt="">
 							</div>
 						</div>
+						<div class="az-firm-right">
+							<span class="az-firm-right1 az-margin-top8 az-xs-margin-top0">Потолок Эверест</span>
+							<span class="az-firm-right2 az-margin-top8">
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-firm-right2-inner">Средняя оценка 4.0</span>
+							</span>
+							<span class="az-firm-right3 az-margin-top8">Москва ул. Савельево 230 офис 32</span>
+							<span class="az-firm-right4 az-margin-top8">8 800 900-30-40</span>
+						</div>
+						<div class="clearfix"></div>
 					</div>
-					<div class="az-firm-right"><br></div>
-					<div class="clearfix"></div>
 				</div>
 				<div class="az-col3">
-					<div class="az-firm-left">
-						<div class="az-img-box_style">
-							<div class="az-img-box2">
-								<img src="img/comp1.jpg" alt="">
+					<div class="az-firm-item az-margin-bottom40 az-zapis-border az-zapis-border_style2">
+						<div class="az-firm-left">
+							<div class="az-img-box2 az-img-box2_style2">
+								<img src="img/comp2.jpg" alt="">
 							</div>
 						</div>
+						<div class="az-firm-right">
+							<span class="az-firm-right1 az-margin-top8 az-xs-margin-top0">Потолок Эверест</span>
+							<span class="az-firm-right2 az-margin-top8">
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-firm-right2-inner">Средняя оценка 4.0</span>
+							</span>
+							<span class="az-firm-right3 az-margin-top8">Москва ул. Савельево 230 офис 32</span>
+							<span class="az-firm-right4 az-margin-top8">8 800 900-30-40</span>
+						</div>
+						<div class="clearfix"></div>
 					</div>
-					<div class="az-firm-right"><br></div>
-					<div class="clearfix"></div>
 				</div>
 				<div class="az-col3">
-					<div class="az-firm-left">
-						<div class="az-img-box_style">
-							<div class="az-img-box2">
+					<div class="az-firm-item az-margin-bottom40 az-zapis-border az-zapis-border_style2">
+						<div class="az-firm-left">
+							<div class="az-img-box2 az-img-box2_style2">
+								<img src="img/comp3.jpg" alt="">
+							</div>
+						</div>
+						<div class="az-firm-right">
+							<span class="az-firm-right1 az-margin-top8 az-xs-margin-top0">Потолок Эверест</span>
+							<span class="az-firm-right2 az-margin-top8">
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-firm-right2-inner">Средняя оценка 4.0</span>
+							</span>
+							<span class="az-firm-right3 az-margin-top8">Москва ул. Савельево 230 офис 32</span>
+							<span class="az-firm-right4 az-margin-top8">8 800 900-30-40</span>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="az-col3">
+					<div class="az-firm-item az-margin-bottom40">
+						<div class="az-firm-left">
+							<div class="az-img-box2 az-img-box2_style2">
+								<img src="img/comp2.jpg" alt="">
+							</div>
+						</div>
+						<div class="az-firm-right">
+							<span class="az-firm-right1 az-margin-top8 az-xs-margin-top0">Потолок Эверест</span>
+							<span class="az-firm-right2 az-margin-top8">
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-firm-right2-inner">Средняя оценка 4.0</span>
+							</span>
+							<span class="az-firm-right3 az-margin-top8">Москва ул. Савельево 230 офис 32</span>
+							<span class="az-firm-right4 az-margin-top8">8 800 900-30-40</span>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="az-col3">
+					<div class="az-firm-item az-margin-bottom40">
+						<div class="az-firm-left">
+							<div class="az-img-box2 az-img-box2_style2">
 								<img src="img/comp1.jpg" alt="">
 							</div>
 						</div>
+						<div class="az-firm-right">
+							<span class="az-firm-right1 az-margin-top8 az-xs-margin-top0">Потолок Эверест</span>
+							<span class="az-firm-right2 az-margin-top8">
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-yellow2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-star-grey2">&nbsp;</span>
+								<span class="az-firm-right2-inner">Средняя оценка 4.0</span>
+							</span>
+							<span class="az-firm-right3 az-margin-top8">Москва ул. Савельево 230 офис 32</span>
+							<span class="az-firm-right4 az-margin-top8">8 800 900-30-40</span>
+						</div>
+						<div class="clearfix"></div>
 					</div>
-					<div class="az-firm-right"><br></div>
-					<div class="clearfix"></div>
 				</div>
 				<div class="clearfix"></div>
 			</div>
