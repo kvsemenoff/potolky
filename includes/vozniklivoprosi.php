0a7e5a7f4cf868d34fb2f9099a79b525636c66f3
engine77 <zaur.ka.00@mail.ru>
2016-10-15 11:07:36 +0300

    w

--- includes/vozniklivoprosi.php
+++ includes/vozniklivoprosi.php
@@ -1,8 +1,9 @@
 <div class="banner_vopros">
-	<div class="zk_banner"><img src="../img/zk_banner.png" alt="" class="img-responsive"></div>
+	
 	<div class="zk_text">
 		<h3>У вас возникли вопросы?</h3>
 		<p>Мы поможем Вам в них разобраться</p>
+		<a href="" class="zk_banner_vop">ЗАДАТЬ ВОПРОС</a>	
 	</div>
-	<a href="" class="zk_banner_vop">ЗАДАТЬ ВОПРОС</a>	
+	
 </div>
