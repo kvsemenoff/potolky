9936df43ec868eeabfeba8750cf2e0d172cd2fb0
unknown <anzarsh@mail.ru>
2016-10-15 13:50:42 +0400

    sdfsd

--- includes/catologcomp.php
+++ includes/catologcomp.php
@@ -1,1 +1,84 @@
-catologcomp.php
+<div class="az-firmi">
+	<div class="az-tovari-lr">
+		<div class="az-tovari-tb">
+			<div class="az-margin-top--20px">
+				<div class="az-tov-head1">
+					<span class="az-tovari-title">Каталог компаний</span>
+				</div>
+				<div class="az-tov-head2">
+					<span class="az-col-firm">Всего <a href="#">80</a> компаний</span>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="az-firm-items">
+				<div class="az-col3">
+					<div class="az-firm-left">
+						<div class="az-img-box_style">
+							<div class="az-img-box2">
+								<img src="img/comp1.jpg" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="az-firm-right"><br></div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="az-col3">
+					<div class="az-firm-left">
+						<div class="az-img-box_style">
+							<div class="az-img-box2">
+								<img src="img/comp1.jpg" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="az-firm-right"><br></div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="az-col3">
+					<div class="az-firm-left">
+						<div class="az-img-box_style">
+							<div class="az-img-box2">
+								<img src="img/comp1.jpg" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="az-firm-right"><br></div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="az-col3">
+					<div class="az-firm-left">
+						<div class="az-img-box_style">
+							<div class="az-img-box2">
+								<img src="img/comp1.jpg" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="az-firm-right"><br></div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="az-col3">
+					<div class="az-firm-left">
+						<div class="az-img-box_style">
+							<div class="az-img-box2">
+								<img src="img/comp1.jpg" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="az-firm-right"><br></div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="az-col3">
+					<div class="az-firm-left">
+						<div class="az-img-box_style">
+							<div class="az-img-box2">
+								<img src="img/comp1.jpg" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="az-firm-right"><br></div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+</div>
